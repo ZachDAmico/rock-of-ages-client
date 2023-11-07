d11f358eb39e559f211426f8b1ab45f6e5fe2f55
Zach D'Amico <zachdamico90@gmail.com>
2023-11-07 13:21:25 -0600

    done

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,44 +1,81 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Authorized } from "./Authorized"
-import { Login } from "../pages/Login.jsx"
-import Home from "../pages/Home"
-import { RockForm } from "./RockForm.jsx"
-import { RockList } from "./RockList.jsx"
-import { Register } from '../pages/Register.jsx'
-
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Authorized } from "./Authorized";
+import { Login } from "../pages/Login.jsx";
+import Home from "../pages/Home";
+import { RockForm } from "./RockForm.jsx";
+import { RockList } from "./RockList.jsx";
+import { Register } from "../pages/Register.jsx";
 
 export const ApplicationViews = () => {
-    const [rocksState, setRocksState] = useState([{
+  const [rocksState, setRocksState] = useState([
+    {
+      id: 1,
+      name: "Sample",
+      type: {
         id: 1,
-        name: "Sample",
-        type: {
-            id: 1,
-            label: "Volcanic"
-        }
-    }])
+        label: "Volcanic",
+      },
+    },
+  ]);
+  // showAll now needed because it's passed up from RockList component
 
-    const fetchRocksFromAPI = async () => {
-        const response = await fetch("http://localhost:8000/rocks",
-            {
-                headers: {
-                    Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
-                }
-            })
-        const rocks = await response.json()
-        setRocksState(rocks)
+  const fetchRocksFromAPI = async (showAll) => {
+    // because URL is going to change depending on showAll true or false, mutable variable and conditional to handle that
+    // original URL will now be default value stored in url variable
+    let url = "http://localhost:8000/rocks";
+    // now checking if showAll IS NOT true, then url is current user
+    if (showAll !== true) {
+      url = "http://localhost:8000/rocks?owner=current";
     }
+    // response is now fetching url variable depending on which condition is met
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Token ${
+          JSON.parse(localStorage.getItem("rock_token")).token
+        }`,
+      },
+    });
+    const rocks = await response.json();
+    setRocksState(rocks);
+  };
 
-    return <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<Authorized />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/allrocks" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/create" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-            </Route>
-        </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<Authorized />}>
+          <Route path="/" element={<Home />} />
+          {/* this is passing rockState and fetchRocks to RockList as props */}
+          <Route
+            path="/allrocks"
+            // showAll allows us to differentiate between all rocks and auth user rocks
+            // it's a prop so needs to be passed elsewhere
+            element={
+              <RockList
+                rocks={rocksState}
+                fetchRocks={fetchRocksFromAPI}
+                showAll={true}
+              />
+            }
+          />
+          <Route
+            path="/create"
+            element={<RockForm fetchRocks={fetchRocksFromAPI} />}
+          />
+          <Route
+            path="/mine"
+            element={
+              <RockList
+                rocks={rocksState}
+                fetchRocks={fetchRocksFromAPI}
+                showAll={false}
+              />
+            }
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
-}
+  );
+};
--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,23 +1,77 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+// showAll is now passed as prop and needed here
+// first step is to make sure right info from API
+// fetchRocks is function that is getting that info
+export const RockList = ({ rocks, fetchRocks, showAll }) => {
+  useEffect(() => {
+    // need fetchRocks() invoked here to show up on page upon render, but don't forget it's defined in ApplicationViews as fetch
+    // so now that fetch component needs to know if it's fetching all rocks or just auth user's
+    // changing fetch URL depending on whether showAll is true or false
+    // that info from fetchRocks function call needs to be passed UP to fetch so showAll is needed as argument here
+    // dependency array is originally empty so the fetchRocks only happens on initial render and not when the page/url changes from mine to allrocks
+    //  so need to acknowledge prop has changed so fetchRock happens when route changes
+    fetchRocks(showAll);
+  }, [showAll]);
 
-export const RockList = ({ rocks, fetchRocks }) => {
-    useEffect(() => {
-    }, [])
+  const displayRocks = () => {
+    if (rocks && rocks.length) {
+      return rocks.map((rock) => (
+        <div
+          key={`key-${rock.id}`}
+          className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+        >
+          <div>
+            {rock.name} ({rock.type.label})
+          </div>
+          <div>
+            In the collection of {rock.user?.first_name} {rock.user?.last_name}
+          </div>
+          {/* without conditional, delete button shows up on all rocks as well and we want user to only have ability to delete own rocks */}
+          {/* checking to see if showAll is true or false and what to render in both cases */}
+          {/* when showAll is true, we want to show no delete button, when showAll is false, we want the button, so the entire <div/> containing button moved to the "or" */}
+          {showAll ? (
+            ""
+          ) : (
+            <div>
+              <button
+                onClick={async () => {
+                  // delete needs to be single rock url, not all rocks
+                  // rock variable above in map is holding current rock, can use rock.id
+                  const response = await fetch(
+                    ` http://localhost:8000/rocks/${rock.id}`,
+                    {
+                      method: "Delete",
+                      headers: {
+                        Authorization: `Token ${
+                          JSON.parse(localStorage.getItem("rock_token")).token
+                        }`,
+                      },
+                    }
+                  );
 
-    const displayRocks = () => {
-        if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
-        }
-
-        return <h3>Loading Rocks...</h3>
+                  // with code above, delete with work, but auto rerender does not, hence code below
+                  if (response.status === 204) {
+                    // needs showAll again to make sure it targets correct list to render
+                    fetchRocks(showAll);
+                  }
+                }}
+                className="border border-solid bg-red-700 text-white p-1"
+              >
+                Delete
+              </button>
+            </div>
+          )}
+        </div>
+      ));
     }
 
-    return (
-        <>
-            <h1 className="text-3xl">Rock List</h1>
-            {displayRocks()}
-        </>
-    )
-}
+    return <h3>Loading Rocks...</h3>;
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl">Rock List</h1>
+      {displayRocks()}
+    </>
+  );
+};
